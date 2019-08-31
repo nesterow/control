aba3107fe043e8e0283a7ef8c6fd478c0e7e4690
Anton Nesterov <arch.nesterov@gmail.com>
2019-08-31 12:52:45 +0300

    Initial commit

---
+++ src/main/java/io/frontless/Controller.java
@@ -0,0 +1,25 @@
+package io.frontless;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javax.imageio.ImageIO;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ImageView imageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
